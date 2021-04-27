8ab854af929d897c7fd0126450dbb27b6e2b0d07
Navpreet Kaur <Navpreet@aedo.ai>
2021-04-27 10:20:55 -0400

    ccvalidate

---
+++ client/src/CreditCardValidator.js
@@ -0,0 +1,23 @@
+import React, { Fragment, useState} from "react";
+import Cards from 'react-credit-cards';
+/**
+ * @author @navpreet
+ * @description
+ **/
+const CreditCardValidator =() =>{
+
+    const [ccnumber, setCCnumber]= useState();
+    const [ccexpiry, setCCexpiry]= useState();
+    const [ccname, setCCname]= useState();
+    const [ccCVC, setCCcvc]= useState();
+
+    const handleCardValidator =()=>{
+    }
+
+    return (
+        <Fragment>
+   
+        </Fragment>);
+}
+
+export default CreditCardValidator
